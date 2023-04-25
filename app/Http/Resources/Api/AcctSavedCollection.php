e8ac6c76d2999e78e4948a5ce900e2e2a62e9c51
amir karimi <takfashomal@gmail.com>
2023-04-25 12:03:04 +0330

    update

---
+++ app/Http/Resources/Api/AcctSavedCollection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Morilog\Jalali\Jalalian;
+
+class AcctSavedCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => $this->collection->map(function($item){
+                return [
+                   'id' => $item->id,
+                   'creator' => $item->creator,
+                   'groups' => $item->groups,
+                   'by' => $item->by ? ['id'=> $item->by->id ,'name' => $item->by->name] : '---',
+                   'created_at' => Jalalian::forge($item->created_at)->__toString(),
+                ];
+            })
+        ];
+    }
+}
