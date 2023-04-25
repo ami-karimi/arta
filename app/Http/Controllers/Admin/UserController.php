2cb40e72c47773ac0dce20c4b79c6df30e87c1fb
amir karimi <takfashomal@gmail.com>
2023-04-25 12:32:37 +0330

    update

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -444,7 +444,7 @@ class UserController extends Controller
         return new AdminActivityCollection($activitys->orderBy('id','DESC')->paginate($per_page));
     }
     public function AcctSaved(Request $request){
-        $savedAccounts = AcctSaved::orderBy('id','DESC')->groupBy('groups')->paginate(20);
+        $savedAccounts = AcctSaved::select('*')->orderBy('id','DESC')->groupBy('groups')->paginate(20);
 
         return response()->json($savedAccounts);
         return new AcctSavedCollection($savedAccounts->paginate(20));
