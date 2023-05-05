7d845c4107e9a19a22af3d9813eda7990355fe46
amir karimi <takfashomal@gmail.com>
2023-05-06 02:10:27 +0330

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,7 +8,7 @@ use App\Utility\V2rayApi;
 class ApiController extends Controller
 {
     public function index(){
-        $login = new V2rayApi("128.140.91.159","4454","amirtld","Amir@###1401");
+        $login = new V2rayApi("128.140.91.159","443","amirtld","Amir@###1401");
 
         $data = $login->status();
 
