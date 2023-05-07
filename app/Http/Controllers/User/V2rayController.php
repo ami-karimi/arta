527e98a2f5d858b6840b5e9e7925f7bfc766c598
amir karimi <takfashomal@gmail.com>
2023-05-07 12:33:01 +0330

    update

---
+++ app/Http/Controllers/User/V2rayController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class V2rayController extends Controller
+{
+   public function buy_volume(Request $request){
+
+   }
+
+   public function update_config(Request $request){
+
+   }
+}
