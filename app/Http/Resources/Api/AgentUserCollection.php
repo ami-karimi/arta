a04be387a4403fd551f690359d1a15867f9f5e99
amir karimi <takfashomal@gmail.com>
2023-05-23 16:45:55 +0330

    update

--- app/Http/Resources/Api/AgentUserCollection.php
+++ app/Http/Resources/Api/AgentUserCollection.php
@@ -55,7 +55,7 @@ class AgentUserCollection extends ResourceCollection
         $map_price = $listGroup->map(function($item){
             $findS = PriceReseler::where('group_id',$item->id)->where('reseler_id',auth()->user()->id)->first();
             return [
-                'group_id' => $item->id,
+                'id' => $item->id,
                 'name' => $item->name,
                 'price_for_reseler' => $item->price_reseler,
                 'reseler_price' => ($findS ? $findS->price : $item->price_reseler),
