9a314be54d5d0a0d2a20889372a8e91b85d3a4fb
amir karimi <takfashomal@gmail.com>
2023-06-19 21:19:33 +0330

    updates

--- app/Http/Controllers/Mobile/AuthController.php
+++ app/Http/Controllers/Mobile/AuthController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Mobile;
 
 use App\Http\Controllers\Controller;
+use http\Env\Response;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Utility\Tokens;
+use Carbon\Carbon;
+use Morilog\Jalali\Jalalian;
 
 class AuthController extends Controller
 {
@@ -22,8 +26,28 @@ class AuthController extends Controller
                 return response()->json(['error' => true, 'result' => 'حساب کاربری شما غیر فعال میباشد لطفا با مدیر تماس بگیرید!']);
             }
 
+            $token = new Tokens();
+            $ts = $token->CreateToken($findUser->id);
+            $expire_date = $findUser->expire_date ;
+            if(!$findUser->expire_set){
+                $findUser->expire_set = 1;
+                $findUser->expire_date = Carbon::now()->addMinutes($findUser->exp_val_minute);
+                $expire_date = $findUser->expire_date;
+                $findUser->first_login = Carbon::now();
+                $findUser->save();
+            }
 
-
+            return  response()->json([
+               'success' => true,
+               'result' =>  [
+                  'token' => $ts->token,
+                  'email' => $findUser->username,
+                  'password' => $findUser->password,
+                  'isPremium' => 0,
+                  'subscriptionEndDate' => Jalalian::forge($expire_date)->__toString(),
+                  'left_day' => Carbon::now()->diffInDays($expire_date, false),
+                 ]
+            ]);
         }
 
         return response()->json(['error' => true, 'result' => 'حساب کابری شما یافت نشد!']);
