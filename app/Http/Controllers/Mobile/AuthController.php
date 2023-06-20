0d22f5d53227e01e525b281d7700cc1572ebfe29
amir karimi <takfashomal@gmail.com>
2023-06-20 04:36:12 +0330

    updates

--- app/Http/Controllers/Mobile/AuthController.php
+++ app/Http/Controllers/Mobile/AuthController.php
@@ -103,10 +103,7 @@ verb 3
 remote s1.arta20.xyz 110
 auth SHA1
 cipher AES-256-CBC
-<auth-user-pass>
-amirtld
-1020
-</auth-user-pass>
+
 <ca>
 -----BEGIN CERTIFICATE-----
 MIIDDzCCAfegAwIBAgIIEQM4BZdb5+0wDQYJKoZIhvcNAQELBQAwDTELMAkGA1UE
