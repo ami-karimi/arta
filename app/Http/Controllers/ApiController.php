d99c3aafc951998bff0533a0579aa41de5d8c846
Amir <takfashomal@gmail.com>
2023-11-28 03:16:31 +0330

    updates

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -51,8 +51,27 @@ class ApiController extends Controller
 
     public function index(){
 
+        $data = User::whereHas('group',function ($query){
+            $query->where('group_type','volume');
+        })->where('limited',0)->get();
+        foreach ($data as $item){
+            $findUser = RadAcct::where('acctstoptime','!=',NULL)->where('saved',0)->where('username',$item->username)->selectRaw('sum(acctoutputoctets) as upload_sum, sum(acctinputoctets) as download_sum, sum(acctinputoctets + acctoutputoctets) as total_sum,username,radacctid')->groupBy('username')->limit(1000)->get();
 
-        Helper::get_db_backup();
+            print_r($findUser);
+            if(count($findUser)) {
+                $item->usage += $findUser[0]['download_sum'] + $findUser[0]['upload_sum'];
+                $item->download_usage += $findUser[0]['download_sum'];
+                $item->upload_usage += $findUser[0]['upload_sum'];
+                if($item->usage >= $item->max_usage ){
+                    $item->limited = 1;
+                }
+                $item->save();
+                RadAcct::where('username',$item->username)->where('saved',0)->update(['saved' => 1]);
+            }
+
+        }
+
+       // Helper::get_db_backup();
        // Helper::get_backup();
 
 
