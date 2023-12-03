e3ef86fb218bbc8c9212ac4ae4321b4e2d49a98c
Amir <takfashomal@gmail.com>
2023-12-03 11:05:29 +0330

    updates

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -1191,7 +1191,7 @@ class UserController extends Controller
         }
 
         $monitor = new MonitorigController() ;
-        if($monitor->KillUser($find->servername->l2tp_address,$find->username)) {
+        if($monitor->KillUser($find->servername,$find->username)) {
             $find->acctstoptime = Carbon::now('Asia/Tehran');
             $find->save();
             return response()->json([
--- app/Http/Controllers/Agent/UserController.php
+++ app/Http/Controllers/Agent/UserController.php
@@ -1051,7 +1051,7 @@ class UserController extends Controller
             ],403);
         }
         $monitor = new MonitorigController() ;
-        if($monitor->KillUser($find->servername->l2tp_address,$find->username)) {
+        if($monitor->KillUser($find->servername,$find->username)) {
             $find->acctstoptime = Carbon::now('Asia/Tehran');
             $find->save();
             return response()->json([
