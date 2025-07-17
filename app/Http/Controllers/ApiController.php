50b5a89071e843d90ffc3c9734ad918ef367881e
mac <takfashomal@gmail.com>
2025-07-18 03:10:18 +0330

    up

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -45,7 +45,7 @@ class ApiController extends Controller
         $server = new WireGuard(62, 'null');
         $peers = $server->getAllPeers();
 
-        return response()->json($peers);
+        return response()->json($peers['user']);
 
 
         /*
